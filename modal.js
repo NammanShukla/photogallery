1b3ac0ba12cb41cadf5f8b61168abb0dfcac74d1
NammanShukla <namman.shukla@gmail.com>
2025-05-16 13:35:57 +0530

    Add Modals

---
+++ modal.js
@@ -0,0 +1,13 @@
+function openModal(image) {
+    const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modalImg")
+
+    modal.style.display = "block"
+    modalImg.src = image.src;
+}
+
+function closeModal(){
+    const modal = document.getElementById("modal"); 
+    modal.style.display = "none";
+}
+
